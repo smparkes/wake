dbeeafaac2a7ae66407cbc873b3c375364d70cad
Steven Parkes <smparkes@smparkes.net>
2010-03-18 13:43:29 -0700

    nil means never repeat if not outdated

--- lib/wake/graph/node/file.rb
+++ lib/wake/graph/node/file.rb
@@ -1,10 +1,7 @@
 require 'wake/graph/node'
 
 class Wake::Graph::Node::File < Wake::Graph::Node
-
-  # not 100% sure of this ... used by simple plugins, e.g., haml, sass
   def succeeded
-    true
+    nil
   end
-
 end
